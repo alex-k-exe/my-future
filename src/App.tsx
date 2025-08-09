af4154ec1e218a97a5147db54a3144105fd0c471
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-08-09 14:27:30 +1000

    Removed the random divs that the other Alex K totally added

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,11 @@ import Home from "./pages/Home.tsx";
 function App() {
     return (
         <Router>
-            <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-                <div className="w-full max-w-4xl">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<LoginForm />} />
-                        <Route path="/register" element={<RegisterForm />} />
-                    </Routes>
-                </div>
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegisterForm />} />
+            </Routes>
         </Router>
     );
 }
