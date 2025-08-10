7fc7b92f421b1ecdab6449cc669633d331362fa7
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2025-08-10 14:29:56 +1000

    Added fetchApi to AppContext

--- src/lib/AppContext.ts
+++ src/lib/AppContext.ts
@@ -2,5 +2,17 @@ import { createContext } from "react";
 import type { Context } from "../types";
 
 export const AppContext = createContext({
-    user: null
+    user: null,
+    fetchApi: async (endpoint: `/${string}`): Promise<Response> => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            throw new Error("No token found");
+        }
+
+        return fetch(`https://my-future-backend.onrender.com${endpoint}`, {
+            headers: {
+                Cookie: `token=${token}`
+            }
+        });
+    }
 } as Context);
